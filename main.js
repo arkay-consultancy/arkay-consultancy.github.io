8cf3e88e0f28cd67cb3cd056a592c0628cde9419
Mani Kumar Reddy Kancharla <kmanikumarreddy@live.com>
2017-04-04 01:24:56 +0530

    UI bugfixes and cleanup

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-!function(t){"use strict";t(".service-box").click(function(e){e.preventDefault(),t(this).tab("show")}),t(".service-box.scroll").on("click",function(e){var o=t(this).attr("data-target");t("html, body").animate({scrollTop:t(o).offset().top},"slow"),e.preventDefault()}),t("a.page-scroll").bind("click",function(e){var o=t(this);t("html, body").stop().animate({scrollTop:t(o.attr("href")).offset().top-50},1250,"easeInOutExpo"),e.preventDefault()}),t("body").scrollspy({target:".navbar-fixed-top",offset:51}),t(".navbar-collapse ul li a").click(function(){t(".navbar-toggle:visible").click()}),t("#mainNav").affix({offset:{top:100}}),(new WOW).init()}(jQuery);
+! function(t) {
+ "use strict";
+ t(".service-box").click(function(e) {
+  e.preventDefault(), t(this).tab("show")
+ }), t("a.page-scroll").bind("click", function(e) {
+  var o = t(this);
+  t("html, body").stop().animate({
+   scrollTop: t(o.attr("href")).offset().top - 50
+  }, 1250, "easeInOutExpo"), e.preventDefault()
+ }), t("body").scrollspy({
+  target: ".navbar-fixed-top",
+  offset: 51
+ }), t(".navbar-collapse ul li a").click(function() {
+  t(".navbar-toggle:visible").click()
+ }), t("#mainNav").affix({
+  offset: {
+   top: 100
+  }
+ }), (new WOW).init()
+}(jQuery);
